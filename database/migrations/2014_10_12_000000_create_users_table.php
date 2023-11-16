3add2a2261370740e86213cff1d22fb2283d1013
dev-taherm <tahermahram0@gmail.com>
2023-11-16 08:41:39 -1000

    editing user model + m+ f name to username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_seller')->default(false);
+            $table->boolean('is_customer')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
