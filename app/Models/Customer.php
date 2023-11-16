4c7e1e79fa7d6ede77d93239b038a416ff1ce748
dev-taherm <tahermahram0@gmail.com>
2023-11-16 09:19:04 -1000

    adding customer scheme with find customer/s

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Customer extends Model
 {
@@ -16,8 +17,9 @@ class Customer extends Model
         'phone',
         'bio'
     ];
-     public function user()
-    {
+     public function user(): BelongsTo{
+    
         return $this->belongsTo(User::class);
     }
+
 }
