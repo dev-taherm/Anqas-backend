a0bd146b35a7553e6e1e066103ddd6bb0273e30b
dev-taherm <tahermahram0@gmail.com>
2023-11-18 09:00:42 -1000

    add postr secheme and reslover

---
+++ app/GraphQL/Mutations/addPostR.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\PostR;
+
+class addPostR
+{
+    public function __invoke($root, array $args)
+    {
+        $user = Auth::user();
+
+        // Ensure that a user is logged in
+        if (!$user) {
+            throw new \Exception('User is not authenticated.');
+        }
+
+        // Find the customer for the user
+        $customer = $user->customer;
+        $postr = $customer->postr;
+
+       
+         $postr = new PostR();
+        $postr->customer_id = $customer->id;
+        $postr->titel = $args['titel'];
+        $postr->descriptions = $args['descriptions'];
+        $postr->post_address = $args['post_address'];
+        $postr->post_status = $args['post_status'];
+      
+
+        // Save the postr
+        $postr->save();
+
+        $message =  'post added successfully.' ;
+
+        return [
+            'postr' => $postr,
+            'message' => $message,
+        ];
+    }
+}
