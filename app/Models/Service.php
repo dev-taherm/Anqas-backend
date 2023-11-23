aaab4a426e421a378faa652913f1ac3ba4c45668
dev-taherm <tahermahram0@gmail.com>
2023-11-23 08:27:51 -1000

    fillable atributes in sevice  model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
-
+  protected $fillable = [
+        'titel',
+        'descriptions',
+        'address',
+        'status',
+    ];
 
          public function seller()
 {
