e9bc9bc781cc8704a311f72940378e9604fc5e7a
dev-taherm <tahermahram0@gmail.com>
2023-11-24 07:48:10 -1000

    subcatgory model

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -5,27 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SubCategory extends Model
+class Subcategory extends Model
 {
     use HasFactory;
 
-
-      protected $fillable = [
-        "name",
-        "descriptions",
-    ];
-
-       public function category()
+        public function category()
 {
     return $this->belongsTo(Category::class);
 }
-       public function requests()
+
+    public function requests()
 {
-    return $this->hasMany(Request::class);
+    return $this->belongsToMany(Request::class);
 }
 
-     public function services()
+    public function services()
 {
-    return $this->hasMany(Service::class);
+    return $this->belongsToMany(Service::class);
 }
 }
