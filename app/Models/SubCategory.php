7713682d9a39712cd31b97974f0a52f86d3ca0c8
dev-taherm <tahermahram0@gmail.com>
2023-11-24 05:36:56 -1000

    Creating category and subcategory models and setting the relationship between them

---
+++ app/Models/SubCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubCategory extends Model
+{
+    use HasFactory;
+
+
+       public function category()
+{
+    return $this->belongsTo(Category::class);
+}
+       public function requests()
+{
+    return $this->hasMany(Request::class);
+}
+
+     public function services()
+{
+    return $this->hasMany(Service::class);
+}
+}
