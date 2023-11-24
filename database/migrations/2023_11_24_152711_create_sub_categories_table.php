0b95ad0810ec7239855dfb9534b9976359db6465
dev-taherm <tahermahram0@gmail.com>
2023-11-24 05:49:57 -1000

    fill the migration file catgort and subcatcory with artbiutes

--- database/migrations/2023_11_24_152711_create_sub_categories_table.php
+++ database/migrations/2023_11_24_152711_create_sub_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('descriptions');
             $table->timestamps();
         });
     }
